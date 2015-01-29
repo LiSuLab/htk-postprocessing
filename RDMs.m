cee7cb0d9451421e933e59b4abec30c45d347d09
Cai Wingfield <cw417@cam.ac.uk>
2015-01-29 12:02:29 +0000

    Save figures with titles.

--- RDMs.m
+++ RDMs.m
@@ -1,9 +1,18 @@
 clear;
+close all;
+
+here = pwd;
+mkdir('Figures');
 
 %% Load in the features
 
 features = load('Features.mat');
 condition_names = fieldnames(features);
+
+userOptions = struct();
+userOptions.saveFiguresJpg = true;
+userOptions.analysisName = 'cepstral';
+userOptions.rootPath = here;
 
 %% Rearrange into feature vectors per frame
 
@@ -12,13 +21,9 @@ condition_count = length(condition_names);
 
 % Preallocate
 RDMs_per_frame = NaN(condition_count, condition_count, frame_count);
+patterns = NaN(condition_count, feature_count);
 
 for frame_i = 1:frame_count
-    
-    % Preallocate
-    % TODO: move this outside loop
-    patterns = NaN(condition_count, feature_count);
-    
     for condition_i = 1:condition_count
         condition = condition_names{condition_i};
         patterns(condition_i,:) = features.(condition)(frame_i,:);
@@ -30,5 +35,12 @@ end
 %% Display RDMs
 
 for rdm_i = 1:size(RDMs_per_frame, 3)
-    showRDMs(RDMs_per_frame(:,:,rdm_i), rdm_i ,true, [], true, 2/3, [], 'Jet');
+    this_RDM = squareform(RDMs_per_frame(:,:,rdm_i));
+    this_RDM = scale01(tiedrank(this_RDM));
+    rdm_title = sprintf('frame%03d', rdm_i-1);
+    % make into struct to add a title
+    s_RDM = struct('name', rdm_title, 'RDM', squareform(this_RDM));
+    % we've pre-rank-transformed them for speed
+    showRDMs(s_RDM, rdm_i, false, [], true, 2/3, [], 'Jet');
+    handleCurrentFigure(fullfile(here, 'Figures', rdm_title), userOptions)
 end
