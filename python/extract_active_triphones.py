9c5699072a5448459bfa56e20dc563c472bcd519
Cai Wingfield <cw417@cam.ac.uk>
2015-02-06 15:17:20 +0000

    Apply active triphone model.

--- python/extract_active_triphones.py
+++ python/extract_active_triphones.py
@@ -8,13 +8,13 @@ import re
 from cw_common import *
 
 
-def get_triphone_lists(input_filename, frames, silent):
+def get_triphone_lists(input_filename, frame_cap, silent):
 
     # Regular expression for the path of a word
     """
     Want to retun a word-keyed dictionary of frame_id-keyed dictionaries of triphone lists.
     :param input_filename:
-    :param frames:
+    :param frame_cap:
     :param silent:
     """
     word_path_re = re.compile(r"^File: (?P<wordpath>.+)\.mfc$")
@@ -65,7 +65,7 @@ def get_triphone_lists(input_filename, frames, silent):
 
                 # If the frame_id we've extracted is larger than our
                 # user-specified cap, we can skip this one.
-                if int(frame_id) >= frames:
+                if int(frame_id) >= frame_cap:
                     continue
 
                 # Otherwise continue to break down the list of triphones which
@@ -99,20 +99,76 @@ def process_args(switches, parameters, commands):
     silent = "S" in switches
     log = "l" in switches
 
-    input_file = get_parameter(parameters, "input", True, usage_text)
-    output_file = get_parameter(parameters, "output", True, usage_text)
+    input_filename = get_parameter(parameters, "input", True, usage_text)
+    output_filename = get_parameter(parameters, "output", True, usage_text)
+    wordlist_filename = get_parameter(parameters, "words", True, usage_text)
 
-    frames = get_parameter(parameters, "frames", usage_text=usage_text)
+    frame_cap = get_parameter(parameters, "frames", usage_text=usage_text)
 
     # set defaults
-    frames = frames if frames != "" else 20 # default of 20
+    frame_cap = frame_cap if frame_cap != "" else 20 # default of 20
 
-    return silent, log, input_file, output_file, frames
+    return silent, log, input_filename, output_filename, wordlist_filename, frame_cap
+
+
+def apply_active_triphone_model(words_data, word_list, frame_cap):
+    """
+    The active triphone model will be calculated as follows.
+
+    - There will be one model for each phone.
+    - The models will give, for each frame and each words, a count of the
+      active triphones with the current phone as the centre phone.
+
+    So to be returned is a phone-keyed dictionary of frame_id-keyed
+    dictionaries of word-keyed dictionaries of counts
+
+    :param words_data:
+    """
+    phone_list = ["sil", "sp", "ax", "k", "ao", "d", "ia", "n", "ae", "r", "b", "t", "ea", "p", "l", "ey", "ih", "g", "m", "y", "uh", "s", "ng", "aa", "ow", "sh", "eh", "zh", "iy", "v", "ch", "jh", "ay", "uw", "th", "z", "hh", "er", "oh", "ah", "aw", "oy", "dh", "f", "ua", "w"]
+
+    phones_data = dict()
+
+    for phone in phone_list:
+
+        frames_data = dict()
+
+        for frame in range(frame_cap):
+            frame_id = str(frame)
+
+            for word in word_list:
+
+                triphone_list = words_data[word][frame_id]
+
+                count = 0
+                for triphone in triphone_list:
+
+                    phone_triplet = triphone.replace('-', ' ').replace('+', ' ').split(' ')
+
+                    if phone_triplet == phone:
+                        count += 1
+
+                words_data[word] = count
+
+            frames_data[frame_id] = words_data
+
+        phones_data[phone] = frames_data
+
+    return phones_data
+
+
+
+def get_word_list(wordlist_filename):
+    with open(wordlist_filename, encoding="utf-8") as word_list_file:
+        for word in word_list_file:
+            yield word
 
 
 def main(argv):
     (switches, parameters, commands) = parse_args(argv)
-    (silent, log, input_file, output_file, c_list, d_list, a_list, frames) = process_args(switches, parameters, commands)
+    (silent, log, input_filename, output_file, wordlist_filename, frame_cap) = process_args(switches, parameters, commands)
+    word_list = get_word_list(wordlist_filename)
+    word_data = get_triphone_lists(input_filename, frame_cap, silent)
+    phones_data = apply_active_triphone_model(word_data, word_list, frame_cap)
 
 if __name__ == "__main__":
    with open(get_log_filename(__file__), mode="a", encoding="utf-8") as log_file, RedirectStdoutTo(log_file):
