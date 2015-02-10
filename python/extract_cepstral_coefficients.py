fe335e2a39b411f9079a59574d3122a0700f2c6c
Cai Wingfield <cw417@cam.ac.uk>
2015-02-10 13:19:28 +0000

    Put into one file.

--- python/extract_cepstral_coefficients.py
+++ python/extract_cepstral_coefficients.py
@@ -4,6 +4,9 @@ Extract some cepstral coefficients from HTK's output file.
 """
 
 import re
+import numpy
+import scipy
+import scipy.io
 
 from cw_common import *
 
@@ -108,6 +111,122 @@ def filter_coefficients(input_filename, output_filename, c_list, d_list, a_list,
 
                         output_file.write(line_to_write)
 
+
+def get_condition_vectors(input_filename, word_list, silent):
+    """
+    Expect frames to start at 0 for each condition and to be sequential
+    in increments of 1
+
+    :param input_filename:
+    :param word_list:
+    :param silent:
+    :return condition_vectors: a word-keyed dictionary of
+            (frame, condition)-arrays
+    :return condition_labels: a dictionary of ints, indexed by condition
+            labels.
+    """
+    condition_label_re = re.compile(r"^(?P<conditionlabel>[a-z]+)$")
+    feature_vector_re = re.compile(r"^(?P<frameid>[0-9]+):(?P<featurevector>.*)$")
+
+    condition_vectors = dict()
+    this_condition_label = None
+
+    # Defaults
+    this_condition_array = None
+
+    with open(input_filename, encoding="utf-8") as input_file:
+
+        for line in input_file:
+
+            # Possible matches
+            condition_label_match = condition_label_re.match(line)
+            feature_vector_match = feature_vector_re.match(line)
+
+            # The line we've read can match either condition labels
+            # or feature vectors for a particular frame
+            if condition_label_match:
+
+                # We've matched a new condition label
+
+                # If there's a condition that's already been processed,
+                # we should remember what we've got so far
+                # BUT we only need to do this if it's on the approved word
+                # list!
+                if (this_condition_label is not None) and (this_condition_label in word_list):
+                    condition_vectors[this_condition_label] = this_condition_array
+
+                # Reset defaults
+                this_condition_array = None
+
+                # Now we can start on the newly processed label
+                this_condition_label = condition_label_match.group("conditionlabel")
+
+                if not silent:
+                    prints("Condition: {0}".format(this_condition_label))
+
+            elif feature_vector_match:
+
+                # We've matched a feature vector for a particular frame
+
+                this_frame_id = feature_vector_match.group("frameid")
+                this_condition_vector = [float(x) for x in feature_vector_match.group("featurevector").split(",")]
+
+                if not silent:
+                    prints("\tf-{0}:".format(this_frame_id))
+                    i = 1
+                    for feature in this_condition_vector:
+                        prints("\t\t[{0}]{1}".format(i, feature))
+                        i += 1
+
+                # If this is the first frame for this condition, we need to
+                # create an array for the condition vector
+                if this_condition_array is None:
+                    this_condition_array = numpy.array(this_condition_vector)
+
+                # Otherwise we need to append the current list to the array
+                else:
+                    # Just quick sanity check
+                    if this_frame_id == "0" or this_frame_id is None:
+                        raise ApplicationError("Wasn't expecting the first frame to be here")
+
+                    this_condition_array = numpy.vstack((
+                        this_condition_array,
+                        numpy.array(this_condition_vector)
+                    ))
+
+        # Remember to save what we've got on the last one too.
+        # We only need to do this if it's on the approved word list!
+        if this_condition_label in word_list:
+            condition_vectors[this_condition_label] = this_condition_array
+
+    return condition_vectors
+
+
+def transform_and_save(output_filename, condition_vectors):
+    """
+    Will save the following in a Matlab-readable format:
+    - A struct with a field named after each condition label, containing frame
+      x condition arrays
+
+    :param output_filename:
+    :param condition_vectors: a word-keyed dictionary of
+                              (frame, condition)-arrays
+    """
+
+    scipy.io.savemat(output_filename, condition_vectors, appendmat=False)
+
+
+def get_words(words_filename):
+    """
+    Reads a list of words from a text file.
+    :param words_filename:
+    :return:
+    """
+    with open(words_filename, encoding="utf-8") as words_file:
+        lines = words_file.readlines()
+        # Don't want the newlines at the end
+        return [line.rstrip("\n") for line in lines]
+
 # noinspection PyUnusedLocal
 def process_args(switches, parameters, commands):
     """
@@ -126,15 +245,6 @@ def process_args(switches, parameters, commands):
         "D=<DC-list> "
         "A=<AC-list> "
         "frames=<frames|20>"
-        ""
-        "For example:"
-        "python extract_cepstral_coefficients "
-        "input=C:\\Users\\cai\\code\\cepstral-model\\HLIST39cepstral.pre.out "
-        "output=C:\\Users\\cai\\code\\cepstral-model\\ProcessedResult.log "
-        "C=0,1,2,3,4,5,6,7,8,9,10,11,12 "
-        "D=0,1,2,3,4,5,6,7,8,9,10,11,12 "
-        "A=0,1,2,3,4,5,6,7,8,9,10,11,12 "
-        "frames=20"
     )
 
     silent = "S" in switches
