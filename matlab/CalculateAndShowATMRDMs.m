d877d57895f5aca7e2c85bab1bd778cea97905c1
Cai Wingfield <cw417@cam.ac.uk>
2015-02-11 15:12:09 +0000

    Load in features.

--- matlab/CalculateAndShowATMRDMs.m
+++ matlab/CalculateAndShowATMRDMs.m
@@ -1,8 +1,24 @@
 clear;
 close all;
 
-here = pwd;
+%% Paths
+
+% Change these values
+input_dir = fullfile('C:', 'Users', 'cai', 'analyses', 'Lexpro', 'Phonetic_models', 'Active_triphone_models');
+output_dir = fullfile('C:', 'Users', 'cai', 'analyses', 'Lexpro', 'Phonetic_models', 'Active_triphone_models');
+
+chdir(output_dir)
 mkdir('Figures');
+figures_dir = fullfile(output_dir, 'Figures');
+
+
+%% UserOptions
+userOptions = struct();
+userOptions.saveFiguresJpg = true;
+userOptions.displayFigures = false;
+userOptions.analysisName = 'active-triphone';
+userOptions.rootPath = '';
+
 
 %% Get the list of phones and load in each one
 
@@ -11,6 +27,7 @@ file_list = dir('*.mat');
 
 % Preallocate
 phone_list = cell(length(file_list));
+phones = struct();
 
 for file_i = 1:length(file_list)
    this_file_name = file_list(file_i).name;
@@ -22,48 +39,7 @@ for file_i = 1:length(file_list)
    this_phone_name = filename_parts{2};
    phone_list{file_i} = this_phone_name;
    
-   % load this phone's data
+   %% load this phone's data
    phones.(this_phone_name) = load(this_file_name);
 end
 
-
-%% Load in the features
-
-features = load('Features.mat');
-condition_names = fieldnames(features);
-
-userOptions = struct();
-userOptions.saveFiguresJpg = true;
-userOptions.analysisName = 'cepstral';
-userOptions.rootPath = here;
-
-%% Rearrange into feature vectors per frame
-
-[frame_count, feature_count] = size(features.(condition_names{1}));
-condition_count = length(condition_names);
-
-% Preallocate
-RDMs_per_frame = NaN(condition_count, condition_count, frame_count);
-patterns = NaN(condition_count, feature_count);
-
-for frame_i = 1:frame_count
-    for condition_i = 1:condition_count
-        condition = condition_names{condition_i};
-        patterns(condition_i,:) = features.(condition)(frame_i,:);
-    end
-    
-    RDMs_per_frame(:,:,frame_i) = squareform(pdist(patterns, 'Correlation'));
-end
-
-%% Display RDMs
-
-for rdm_i = 1:size(RDMs_per_frame, 3)
-    this_RDM = squareform(RDMs_per_frame(:,:,rdm_i));
-    this_RDM = scale01(tiedrank(this_RDM));
-    rdm_title = sprintf('atm-frame%03d', rdm_i-1);
-    % make into struct to add a title
-    s_RDM = struct('name', rdm_title, 'RDM', squareform(this_RDM));
-    % we've pre-rank-transformed them for speed
-    showRDMs(s_RDM, rdm_i, false, [], true, 2/3, [], 'Jet');
-    handleCurrentFigure(fullfile(here, 'Figures', rdm_title), userOptions)
-end
