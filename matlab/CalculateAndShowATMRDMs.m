affc5a060501d46894e710a187bb8172288826d7
Cai Wingfield <cai@caiwingfield.com>
2015-02-25 11:39:02 +0000

    Tightening up code.

--- matlab/CalculateAndShowATMRDMs.m
+++ matlab/CalculateAndShowATMRDMs.m
@@ -30,8 +30,8 @@ chdir(input_dir);
 file_list = dir('*.mat');
 
 % Preallocate
-phone_list = cell(length(file_list));
-phones = struct();
+phone_list = cell(length(file_list), 1);
+phones_data = struct();
 
 for file_i = 1:length(file_list)
    this_file_name = file_list(file_i).name;
@@ -44,21 +44,21 @@ for file_i = 1:length(file_list)
    phone_list{file_i} = this_phone_name;
    
    % load this phone's data
-   phones.(this_phone_name) = load(this_file_name);
+   phones_data.(this_phone_name) = load(this_file_name);
 end
 
 %% Get some lists and limits
-phones_list = fieldnames(phones);
-phones_list = sort(phones_list);
-word_list = fieldnames(phones.(phones_list{1}));
+phone_list = sort(phone_list);
+word_list = fieldnames(phones_data.(phone_list{1}));
 word_list = sort(word_list);
-n_frames = size(phones.(phones_list{1}).(word_list{1}), 1);
+n_frames = size(phones_data.(phone_list{1}).(word_list{1}), 1);
+
 
 %% Build RDMs
 % We have one RDM for each frame and each phone
 for frame = 1 : n_frames
-    for phone_i = 1 : length(phones_list)
-        this_phone = phones_list{phone_i};
+    for phone_i = 1 : length(phone_list)
+        this_phone = phone_list{phone_i};
         
         this_RDM_name = sprintf('%s frame%d', this_phone, frame);
         
@@ -67,7 +67,7 @@ for frame = 1 : n_frames
         % The RDMs are word-by-word
         for word_i = 1 : length(word_list)
             this_word = word_list{word_i};
-            data_for_this_condition = phones.(this_phone).(this_word)(frame, :);
+            data_for_this_condition = phones_data.(this_phone).(this_word)(frame, :);
             if isnan(data_for_this_RDM)
             	data_for_this_RDM = data_for_this_condition;
             else
