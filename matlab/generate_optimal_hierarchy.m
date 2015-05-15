c5b78800d9c3ef8618e3f8de1a5a69f537bbaa8d
Cai Wingfield <cai@caiwingfield.com>
2015-05-15 14:32:10 +0100

    Added some lines.

--- matlab/generate_optimal_hierarchy.m
+++ matlab/generate_optimal_hierarchy.m
@@ -7,8 +7,8 @@ function branching = generate_optimal_hierarchy()
     %% Phone-feature matrix
     M = [ ...
 ...%    1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40
-        ...%  AA AE AH AO AW AY B  CH D  EA EH ER EY F  G  HH IA IH IY JH K  L  M  N  NG OH OW OY P  R  S  SH T  TH UH UW V  W  Y  Z  
-...%  1. DORSAL
+...%    AA AE AH AO AW AY B  CH D  EA EH ER EY F  G  HH IA IH IY JH K  L  M  N  NG OH OW OY P  R  S  SH T  TH UH UW V  W  Y  Z  
+...%  1. DORSAL  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
         0  0  0  0  0  0  0  0  0  0  0  0  0  0  1  0  0  0  0  0  1  0  0  0  1  0  0  0  0  0  0  1  0  0  0  0  0  0  0  0 ; ...
 ...%  2. CORONAL
         0  0  0  0  0  0  0  0  1  0  0  0  0  0  0  0  0  0  0  0  0  0  0  1  0  0  0  0  0  0  1  0  1  0  0  0  0  0  0  1 ; ...
