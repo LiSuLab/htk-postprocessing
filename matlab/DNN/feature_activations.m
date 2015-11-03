1982bb373dfc2e97b4c52f7a35a3ae3c6ad722ca
Cai Wingfield <cw417@cam.ac.uk>
2015-11-03 14:37:20 +0000

    start feature activation templates function

---
+++ matlab/DNN/feature_activations.m
@@ -0,0 +1,6 @@
+% Produces sliding-window-matched plots for the instances of each phone for
+% each word.
+function [feature_models] = feature_activations()
+    phone_models = phone_activations();
+
+end%function
