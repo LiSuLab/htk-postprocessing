e0606c60387c92bdf6a43ea233ad30513452d69e
Cai Wingfield <cw417@cam.ac.uk>
2016-04-12 12:20:43 +0100

    Multi-model RDM stack production.

--- matlab/DNN/making_models/dynamic_hidden_layer_models.m
+++ matlab/DNN/making_models/dynamic_hidden_layer_models.m
@@ -2,28 +2,42 @@ function dRDMs = dynamic_hidden_layer_models(distance_type)
 
     if ~exist('distance_type', 'var'), distance_type = 'correlation'; end
 
-    bn_activations = load('/Users/cai/Desktop/scratch/py_out/hidden_layer_7BN_activations.mat');
+    node_activations.L2   = load('/imaging/cw04/CSLB/Analysis_DNN/Models/hidden_layer_2_activations.mat');
+    node_activations.L3   = load('/imaging/cw04/CSLB/Analysis_DNN/Models/hidden_layer_3_activations.mat');
+    node_activations.L4   = load('/imaging/cw04/CSLB/Analysis_DNN/Models/hidden_layer_4_activations.mat');
+    node_activations.L5   = load('/imaging/cw04/CSLB/Analysis_DNN/Models/hidden_layer_5_activations.mat');
+    node_activations.L6   = load('/imaging/cw04/CSLB/Analysis_DNN/Models/hidden_layer_6_activations.mat');
+    node_activations.L7BN = load('/imaging/cw04/CSLB/Analysis_DNN/Models/hidden_layer_7BN_activations.mat');
     
-    words = fieldnames(bn_activations);
+    layer_names = fieldnames(node_activations);
+    n_layers = numel(layer_names);
+    
+    words = fieldnames(node_activations.(layer_names{1}));
     n_words = numel(words);
     
+    % Only need this once
     shortest_word_length = inf;
     for word_i = 1:n_words
         word = words{word_i};
-        [word_length, n_bn_nodes] = size(bn_activations.(word));
+        [word_length, n_nodes] = size(node_activations.(layer_names{1}).(word));
         shortest_word_length = min(shortest_word_length, word_length);
     end
     
     dRDMs = struct();
-    for t = 1:shortest_word_length
-       data_this_timepoint = nan(n_words, n_bn_nodes);
-       for word_i = 1:n_words
-           word = words{word_i};
-           word_activation = bn_activations.(word);
-           data_this_timepoint(word_i, :) = word_activation(t, :);
-       end
-       RDM_this_timepoint = pdist(data_this_timepoint, distance_type);
-       dRDMs(t).RDM = RDM_this_timepoint;
+    for l = 1:n_layers
+        layer_name = layer_names{l};
+        n_nodes_this_layer = size(node_activations.(layer_name).(words{1}), 2);
+        for t = 1:shortest_word_length
+           data_this_timepoint = nan(n_words, n_nodes_this_layer);
+           for word_i = 1:n_words
+               word = words{word_i};
+               word_activation = node_activations.(layer_name).(word);
+               data_this_timepoint(word_i, :) = word_activation(t, :);
+           end
+           RDM_this_timepoint = pdist(data_this_timepoint, distance_type);
+           dRDMs(t).(layer_name).RDM = RDM_this_timepoint;
+           dRDMs(t).(layer_name).Name = sprintf('%s,t=%02d', layer_name, t);
+        end
     end
 
 end%function
