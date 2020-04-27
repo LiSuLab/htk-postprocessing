f204194a5d2451f7a11f6f2399ff7529c0d27bc9
Cai Wingfield <cw417@cam.ac.uk>
2020-04-27 11:24:46 +0100

    Add feature matrix

--- python/plot_activations_tsne.py
+++ python/plot_activations_tsne.py
@@ -17,7 +17,7 @@ caiwingfield.net
 from __future__ import annotations
 
 from collections import defaultdict
-from enum import Enum
+from enum import Enum, auto
 from pathlib import Path
 from typing import Dict, List, DefaultDict, Tuple
 
@@ -26,6 +26,7 @@ import scipy.io
 import mat73
 from sklearn.manifold import TSNE
 from matplotlib import pyplot
+from pandas import DataFrame
 
 
 LOAD_DIR = Path("/Users/cai/Dox/Academic/Analyses/Lexpro/DNN mapping/scratch/py_out")
@@ -34,7 +35,7 @@ SAVE_DIR = Path("/Users/cai/Dox/Academic/Analyses/Lexpro/DNN mapping/t-sne/new t
 PERPLEXITY = 40
 
 
-def main():
+def run_tsne_script():
     """The script."""
 
     phone_segmentations = PhoneSegmentationSet.load()
@@ -54,7 +55,7 @@ def main():
 
 
 def plot_tsne(activations_per_point: array,
-              phone_labels_per_point: List[PhoneSegment.Label],
+              phone_labels_per_point: List[Phone],
               figure_name: str):
     """Generate and plot t-SNE"""
     t_sne_positions_path = Path(SAVE_DIR, f"t-sne positions {figure_name}.npz")
@@ -95,9 +96,9 @@ def compute_tsne_positions(activations_per_point: array) -> array:
 
 
 def stack_data_for_layer(layer, phone_segmentations) -> Tuple[
-    array, List[PhoneSegment.Label],
-    array, List[PhoneSegment.Label],
-    Dict[PhoneSegment.Label, array]
+    array, List[Phone],
+    array, List[Phone],
+    Dict[Phone, array]
 ]:
     """
     Load data for the specified layer, and distribute it in various ways.
@@ -119,7 +120,7 @@ def stack_data_for_layer(layer, phone_segmentations) -> Tuple[
     for word in phone_segmentations.words:
         for segment in phone_segmentations[word]:
             # Skip silence
-            if segment.label == PhoneSegment.Label.sil:
+            if segment.label == Phone.sil:
                 continue
             activations_this_segment = layer_activations[word][segment.onset_frame:segment.offset_frame, :]
 
@@ -137,7 +138,7 @@ def stack_data_for_layer(layer, phone_segmentations) -> Tuple[
     # Convert to arrays and do averaging as necessary
     activations_per_frame: array = array(activations_per_frame)
     activations_per_word_phone: array = array(activations_per_word_phone)
-    activations_per_phone: Dict[PhoneSegment.Label, array] = {
+    activations_per_phone: Dict[Phone, array] = {
         phone: mean(activations, 0)
         for phone, activations in activations_per_phone.items()
     }
@@ -169,7 +170,7 @@ class PhoneSegmentationSet:
             word: [
                 PhoneSegment(onset_sample=seg[0][0][0],
                              offset_sample=seg[1][0][0],
-                             label=PhoneSegment.Label.from_name(seg[2][0]))
+                             label=Phone.from_name(seg[2][0]))
                 for seg in from_dict[word][0]
             ]
             for word in from_dict.keys()
@@ -196,8 +197,8 @@ class PhoneSegment:
 
     _samples_per_frame = 100_000
 
-    def __init__(self, onset_sample: int, offset_sample: int, label: PhoneSegment.Label):
-        self.label: PhoneSegment.Label = label
+    def __init__(self, onset_sample: int, offset_sample: int, label: Phone):
+        self.label: Phone = label
 
         # samples
         self.onset_sample: int = onset_sample
@@ -209,58 +210,6 @@ class PhoneSegment:
 
     def __repr__(self):
         return f"PhoneSegment(onset_frame={self.onset_frame}, offset_frame={self.offset_frame}, label={self.label})"
-
-    class Label(Enum):
-        """Represents the different phone labels"""
-        sil = 0
-        aa  = 1
-        ae  = 2
-        ah  = 3
-        ao  = 4
-        aw  = 5
-        ay  = 6
-        b   = 7
-        ch  = 8
-        d   = 9
-        ea  = 10
-        eh  = 11
-        er  = 12
-        ey  = 13
-        f   = 14
-        g   = 15
-        hh  = 16
-        ia  = 17
-        ih  = 18
-        iy  = 19
-        jh  = 20
-        k   = 21
-        l   = 22
-        m   = 23
-        n   = 24
-        ng  = 25
-        oh  = 26
-        ow  = 27
-        oy  = 28
-        p   = 29
-        r   = 30
-        s   = 31
-        sh  = 32
-        t   = 33
-        th  = 34
-        ua  = 35
-        uh  = 36
-        uw  = 37
-        v   = 38
-        w   = 39
-        y   = 40
-        z   = 41
-
-        @classmethod
-        def from_name(cls, name: str):
-            for pl in cls:
-                if pl.name == name:
-                    return pl
-            raise ValueError(name)
 
 
 PhoneSegmentation = List[PhoneSegment]
@@ -286,5 +235,140 @@ class DNNLayer(Enum):
             return str(self.value)
 
 
+class Phone(Enum):
+    """Represents the different phone labels"""
+    sil = 0
+    aa  = 1
+    ae  = 2
+    ah  = 3
+    ao  = 4
+    aw  = 5
+    ay  = 6
+    b   = 7
+    ch  = 8
+    d   = 9
+    ea  = 10
+    eh  = 11
+    er  = 12
+    ey  = 13
+    f   = 14
+    g   = 15
+    hh  = 16
+    ia  = 17
+    ih  = 18
+    iy  = 19
+    jh  = 20
+    k   = 21
+    l   = 22
+    m   = 23
+    n   = 24
+    ng  = 25
+    oh  = 26
+    ow  = 27
+    oy  = 28
+    p   = 29
+    r   = 30
+    s   = 31
+    sh  = 32
+    t   = 33
+    th  = 34
+    ua  = 35
+    uh  = 36
+    uw  = 37
+    v   = 38
+    w   = 39
+    y   = 40
+    z   = 41
+
+    @classmethod
+    def from_name(cls, name: str):
+        for pl in cls:
+            if pl.name == name:
+                return pl
+        raise ValueError(name)
+    
+    @property
+    def features(self) -> List[Feature]:
+        return [feature 
+                for feature in Feature 
+                if self in feature.phones]
+
+
+class Feature(Enum):
+    sonorant = auto()
+    voiced = auto()
+    syllabic = auto()
+    obstruent = auto()
+    labial = auto()
+    coronal = auto()
+    dorsal = auto()
+    stop = auto()
+    affricate = auto()
+    fricative = auto()
+    sibilant = auto()
+    approximant = auto()
+    nasal = auto()
+    front = auto()
+    central = auto()
+    back = auto()
+    close = auto()
+    close_mid = auto()
+    open_mid = auto()
+    open_ = auto()
+    rounded = auto()
+
+    @property
+    def name(self) -> str:
+        if self == Feature.open_:
+            return "open"
+        else:
+            return super().name
+
+    @property
+    def phones(self) -> List[Phone]:
+        return [Phone.from_name(p)
+                for p in list(self._df[self._df[self.name] == 1].index)]
+        
+    @classmethod
+    def from_name(cls, name: str) -> Feature:
+        for feature in cls:
+            if feature.name == name:
+                return feature
+        raise ValueError(name)
+
+    @property
+    def _df(self) -> DataFrame:
+        df = DataFrame.from_dict(dict([
+            ("Phone",                  ["aa", "ae", "ah", "ao", "aw", "ay", "b", "ch", "d", "ea", "eh", "er", "ey", "f", "g", "hh", "ia", "ih", "iy", "jh", "k", "l", "m", "n", "ng", "oh", "ow", "oy", "p", "r", "s", "sh", "t", "th", "uh", "uw", "v", "w", "y", "z"]),
+            (Feature.sonorant.name,    [   1,    1,    1,    1,    1,    1,   0,    0,   0,    1,    1,    1,    1,   0,   0,    0,    1,    1,    1,    0,   0,   1,   1,   1,    1,    1,    1,    1,   0,   1,   0,    0,   0,    0,    1,    1,   0,   1,   1,   0]),
+            (Feature.voiced.name,      [   1,    1,    1,    1,    1,    1,   1,    0,   1,    1,    1,    1,    1,   0,   1,    0,    1,    1,    1,    1,   0,   1,   1,   1,    1,    1,    1,    1,   0,   1,   0,    0,   0,    0,    1,    1,   1,   1,   1,   1]),
+            (Feature.syllabic.name,    [   1,    1,    1,    1,    1,    1,   0,    0,   0,    1,    1,    1,    1,   0,   0,    0,    1,    1,    1,    0,   0,   0,   0,   0,    0,    1,    1,    1,   0,   0,   0,    0,   0,    0,    1,    1,   0,   0,   0,   0]),
+            (Feature.obstruent.name,   [   0,    0,    0,    0,    0,    0,   1,    1,   1,    0,    0,    0,    0,   1,   1,    1,    0,    0,    0,    1,   1,   1,   1,   1,    1,    0,    0,    0,   1,   1,   1,    1,   1,    1,    0,    0,   1,   0,   0,   1]),
+            (Feature.labial.name,      [   0,    0,    0,    0,    0,    0,   1,    0,   0,    0,    0,    0,    0,   1,   0,    0,    0,    0,    0,    0,   0,   0,   1,   0,    0,    0,    0,    0,   1,   0,   0,    0,   0,    0,    0,    0,   1,   0,   0,   0]),
+            (Feature.coronal.name,     [   0,    0,    0,    0,    0,    0,   0,    1,   1,    0,    0,    0,    0,   0,   0,    0,    0,    0,    0,    1,   0,   1,   0,   1,    0,    0,    0,    0,   0,   1,   1,    1,   1,    1,    0,    0,   0,   0,   1,   1]),
+            (Feature.dorsal.name,      [   0,    0,    0,    0,    0,    0,   0,    0,   0,    0,    0,    0,    0,   0,   1,    0,    0,    0,    0,    0,   1,   0,   0,   0,    1,    0,    0,    0,   0,   0,   0,    0,   0,    0,    0,    0,   0,   1,   0,   0]),
+            (Feature.stop.name,        [   0,    0,    0,    0,    0,    0,   1,    0,   1,    0,    0,    0,    0,   0,   1,    0,    0,    0,    0,    0,   1,   0,   0,   0,    0,    0,    0,    0,   1,   0,   0,    0,   1,    0,    0,    0,   0,   0,   0,   0]),
+            (Feature.affricate.name,   [   0,    0,    0,    0,    0,    0,   0,    1,   0,    0,    0,    0,    0,   0,   0,    0,    0,    0,    0,    1,   0,   0,   0,   0,    0,    0,    0,    0,   0,   0,   0,    0,   0,    0,    0,    0,   0,   0,   0,   0]),
+            (Feature.fricative.name,   [   0,    0,    0,    0,    0,    0,   0,    0,   0,    0,    0,    0,    0,   1,   0,    1,    0,    0,    0,    0,   0,   0,   0,   0,    0,    0,    0,    0,   0,   0,   1,    1,   0,    1,    0,    0,   1,   0,   0,   1]),
+            (Feature.sibilant.name,    [   0,    0,    0,    0,    0,    0,   0,    1,   0,    0,    0,    0,    0,   0,   0,    0,    0,    0,    0,    1,   0,   0,   0,   0,    0,    0,    0,    0,   0,   0,   1,    1,   0,    0,    0,    0,   0,   0,   0,   1]),
+            (Feature.approximant.name, [   0,    0,    0,    0,    0,    0,   0,    0,   0,    0,    0,    0,    0,   0,   0,    1,    0,    0,    0,    0,   0,   1,   0,   0,    0,    0,    0,    0,   0,   1,   0,    0,   0,    0,    0,    0,   0,   1,   1,   0]),
+            (Feature.nasal.name,       [   0,    0,    0,    0,    0,    0,   0,    0,   0,    0,    0,    0,    0,   0,   0,    0,    0,    0,    0,    0,   0,   0,   1,   1,    1,    0,    0,    0,   0,   0,   0,    0,   0,    0,    0,    0,   0,   0,   0,   0]),
+            (Feature.front.name,       [   0,    1,    0,    0,    0,    1,   0,    0,   0,    1,    1,    0,    1,   0,   0,    0,    1,    1,    1,    0,   0,   0,   0,   0,    0,    0,    0,    0,   0,   0,   0,    0,   0,    0,    0,    0,   0,   0,   0,   0]),
+            (Feature.central.name,     [   0,    0,    0,    0,    1,    1,   0,    0,   0,    1,    0,    1,    0,   0,   0,    0,    1,    0,    0,    0,   0,   0,   0,   0,    0,    0,    1,    1,   0,   0,   0,    0,   0,    0,    0,    0,   0,   0,   0,   0]),
+            (Feature.back.name,        [   1,    0,    1,    1,    1,    0,   0,    0,   0,    0,    0,    0,    0,   0,   0,    0,    0,    0,    0,    0,   0,   0,   0,   0,    0,    1,    1,    1,   0,   0,   0,    0,   0,    0,    1,    1,   0,   0,   0,   0]),
+            (Feature.close.name,       [   0,    0,    0,    0,    0,    1,   0,    0,   0,    0,    0,    0,    0,   0,   0,    0,    1,    1,    1,    0,   0,   0,   0,   0,    0,    0,    0,    0,   0,   0,   0,    0,   0,    0,    1,    1,   0,   0,   0,   0]),
+            (Feature.close_mid.name,   [   0,    0,    0,    0,    1,    0,   0,    0,   0,    0,    0,    0,    1,   0,   0,    0,    1,    0,    0,    0,   0,   0,   0,   0,    0,    0,    1,    1,   0,   0,   0,    0,   0,    0,    0,    0,   0,   0,   0,   0]),
+            (Feature.open_mid.name,    [   0,    1,    1,    1,    0,    0,   0,    0,   0,    1,    1,    1,    1,   0,   0,    0,    0,    0,    0,    0,   0,   0,   0,   0,    0,    0,    1,    1,   0,   0,   0,    0,   0,    0,    0,    0,   0,   0,   0,   0]),
+            (Feature.open_.name,       [   1,    0,    0,    0,    1,    1,   0,    0,   0,    0,    0,    0,    0,   0,   0,    0,    0,    0,    0,    0,   0,   0,   0,   0,    0,    1,    0,    0,   0,   0,   0,    0,   0,    0,    0,    0,   0,   0,   0,   0]),
+            (Feature.rounded.name,     [   0,    0,    0,    1,    1,    0,   0,    0,   0,    0,    0,    0,    0,   0,   0,    0,    0,    0,    0,    0,   0,   0,   0,   0,    0,    1,    1,    1,   0,   0,   0,    0,   0,    0,    1,    1,   0,   0,   0,   0]),
+        ]))
+        df.set_index(keys="Phone", drop=True, inplace=True, verify_integrity=True)
+        return df
+
+
+def scratch():
+    print(Feature.sonorant.phones)
+
+
 if __name__ == '__main__':
-    main()
+    run_tsne_script()
