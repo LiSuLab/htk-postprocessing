63fa0a18f1d05c494a26a831f8a99ee254045eac
Cai Wingfield <cw417@cam.ac.uk>
2020-05-14 08:14:33 +0100

    Speed up shuffling by using numpy permutations

--- common/maths.py
+++ common/maths.py
@@ -1,3 +1,12 @@
+from numpy import array
+from numpy.random import permutation
+
+
 def quantile_of_score(a, score, kind='rank'):
     from scipy.stats import percentileofscore
     return percentileofscore(a, score, kind)/100
+
+
+def shuffle(series: array) -> array:
+    shuffled_indices = permutation(series.shape[0])
+    return series[shuffled_indices]
--- permutation_p_values.py
+++ permutation_p_values.py
@@ -14,48 +14,58 @@ caiwingfield.net
 2020
 ---------------------------
 """
-from logging import getLogger
-from random import shuffle
-from typing import List
+from logging import getLogger, basicConfig, INFO
 
-from numpy import array
+from numpy import array, nan, full
 
 from common.layers import load_and_stack_data_for_layer, DNNLayer
-from common.maths import quantile_of_score
-from common.segmentation import PhoneSegmentationSet, Phone
+from common.maths import quantile_of_score, shuffle
+from common.segmentation import PhoneSegmentationSet
 
 logger = getLogger(__name__)
 
-N_PERMUTATIONS = 10# _000
+N_PERMUTATIONS = 10_000
 
 
 def main(layer: DNNLayer):
     phone_segmentations = PhoneSegmentationSet.load()
 
+    # Load data in a very redundant way (but we already have code for it)
     _, _, activations_per_word_phone, labels_per_word_phone, _ = load_and_stack_data_for_layer(layer,
                                                                                                phone_segmentations)
-    true_value = statistic_for_labelling(activations_per_word_phone, labels_per_word_phone)
+    observed_value = statistic_for_labelling(activations_per_word_phone, labels_per_word_phone)
 
-    distribution = []
-    shuffled_labels = labels_per_word_phone.copy()
+    # using numpy for fast shuffling, so labels must be in array of ints (underlying value of Phone)
+    label_array: array = array([l.value for l in labels_per_word_phone])
+
+    # preallocate permutation distribution of cluster stats
+    distribution: array = full(N_PERMUTATIONS, nan)
+
     for permutation_i in range(N_PERMUTATIONS):
-        shuffle(shuffled_labels)
-        distribution.append(statistic_for_labelling(activations_per_word_phone, shuffled_labels))
+        shuffled_labels = shuffle(label_array)
+        distribution[permutation_i] = (statistic_for_labelling(activations_per_word_phone, shuffled_labels))
 
-    p_value = 1 - quantile_of_score(distribution, true_value, kind='strict')
+    p_value = 1 - quantile_of_score(distribution, observed_value, kind='strict')
 
     logger.info(layer.name)
-    logger.info(f"\tcluster statistic: {true_value}")
+    logger.info(f"\tcluster statistic: {observed_value}")
     logger.info(f"\tp-value for {N_PERMUTATIONS} permutations: {p_value}")
-    print(f"{layer.name}"
-          f"cluster statistic:")
 
 
-def statistic_for_labelling(activations_per_word_phone: array, labels: List[Phone]) -> float:
-    # TODO
-    return 0
+def statistic_for_labelling(activations_per_word_phone: array, labels: array) -> float:
+    """
+    Computes a clustering statistic for the set of activations per phone-instance, given a specified set of labels.
+
+    activations_per_word_phone: phone-occurrence x node array of activations
+    labels: phone-occurrence x 1 array of phone labels (as ints, underlying values for Phone enum)
+
+    returns: cluster stat for labelling
+    """
+    # TODO: Use cluster statistic code from Chao.
+    return -1
 
 
 if __name__ == '__main__':
-    for layer in DNNLayer:
-        main(layer)
+    basicConfig(format='%(asctime)s | %(levelname)s | %(module)s | %(message)s', datefmt="%Y-%m-%d %H:%M:%S", level=INFO)
+    for l in DNNLayer:
+        main(l)
