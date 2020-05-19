be967aec847a950ca65de6c43e78e64a5ce48c81
Cai Wingfield <cw417@cam.ac.uk>
2020-05-19 09:40:05 +0100

    Move script and function definitions

--- fisher/gendata.py
+++ fisher/gendata.py
@@ -2,27 +2,18 @@
 ===========================
 Generate data for Fisher test
 ===========================
-
 Dr. Chao Zhang
 ---------------------------
 2020
 ---------------------------
 """
 
-import os
-import sys
-import numpy
-import scipy
-import scipy.io
 import random
+import sys
 
-labmat = sys.argv[1]
-feamat = sys.argv[2]
+import numpy
+import scipy.io
 
-ndim = 3
-means = [-4, -2, 0, 2, 4]
-sdevs = [0.6, 0.6, 0.6, 0.6, 0.6]
-nsamples = [1000, 1000, 1000, 1000, 1000]
 
 def GenRandValues(ndim, mean, sdev, count):
 	samples = []
@@ -33,18 +24,26 @@ def GenRandValues(ndim, mean, sdev, count):
 		samples.append(values)
 	return samples
 
-labdict = {}
-feadict = {}
-index = 0
-for i in range(0, len(means)):
-	values = GenRandValues(ndim, means[i], sdevs[i], nsamples[i])
-	for eachval in values:
-		labdict[str(index)] = numpy.array([i])
-		feadict[str(index)] = numpy.array(eachval)
-		index += 1
 
+if __name__ == '__main__':
 
-scipy.io.savemat(labmat, labdict)
-scipy.io.savemat(feamat, feadict)
-		
+	labmat = sys.argv[1]
+	feamat = sys.argv[2]
 
+	ndim = 3
+	means = [-4, -2, 0, 2, 4]
+	sdevs = [0.6, 0.6, 0.6, 0.6, 0.6]
+	nsamples = [1000, 1000, 1000, 1000, 1000]
+
+	labdict = {}
+	feadict = {}
+	index = 0
+	for i in range(0, len(means)):
+		values = GenRandValues(ndim, means[i], sdevs[i], nsamples[i])
+		for eachval in values:
+			labdict[str(index)] = numpy.array([i])
+			feadict[str(index)] = numpy.array(eachval)
+			index += 1
+
+	scipy.io.savemat(labmat, labdict)
+	scipy.io.savemat(feamat, feadict)
