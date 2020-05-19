c0df8af77197650f88d4296a08e1612de46c05e6
Cai Wingfield <cw417@cam.ac.uk>
2020-05-19 12:05:14 +0100

    Fill in placeholder stat

---
+++ common/logging.py
@@ -0,0 +1,27 @@
+from sys import stdout
+
+
+def print_progress(iteration, total, prefix='', suffix='', decimals=1, bar_length=100):
+    """
+    Call in a loop to create terminal progress bar.
+    Thanks to https://gist.github.com/aubricus/f91fb55dc6ba5557fbab06119420dd6a
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        bar_length  - Optional  : character length of bar (Int)
+    """
+    str_format = "{0:." + str(decimals) + "f}"
+    portion_complete = iteration/float(total)
+    percents = str_format.format(100 * portion_complete)
+    filled_length = int(round(bar_length * portion_complete))
+    bar = '█' * filled_length + '-' * (bar_length - filled_length)
+
+    stdout.write(f'\r{prefix}|{bar}| {percents}%{suffix}'),
+
+    if iteration == total:
+        stdout.write('\n')
+
+    stdout.flush()
--- permutation_p_values.py
+++ permutation_p_values.py
@@ -19,23 +19,29 @@ from logging import getLogger, basicConfig, INFO
 from numpy import array, nan, full
 
 from common.layers import load_and_stack_data_for_layer, DNNLayer
+from common.logging import print_progress
 from common.maths import quantile_of_score, shuffle
 from common.segmentation import PhoneSegmentationSet
+from fisher.fisher import GetFisher
 
 logger = getLogger(__name__)
 
-N_PERMUTATIONS = 10_000
+N_PERMUTATIONS = 5_000
 
 
 def main(layer: DNNLayer):
+
+    logger.info(layer.name)
+
     phone_segmentations = PhoneSegmentationSet.load()
 
     # Load data in a very redundant way (but we already have code for it)
-    _, _, activations_per_word_phone, labels_per_word_phone, _ = load_and_stack_data_for_layer(layer, phone_segmentations)
-    observed_value = statistic_for_labelling(activations_per_word_phone, labels_per_word_phone)
-
+    _, _, activations_per_word_phone, labels_per_word_phone, _ = load_and_stack_data_for_layer(layer,
+                                                                                               phone_segmentations)
     # using numpy for fast shuffling, so labels must be in array of ints (underlying value of Phone)
     label_array: array = array([l.value for l in labels_per_word_phone])
+
+    observed_value = statistic_for_labelling(activations_per_word_phone, label_array)
 
     # preallocate permutation distribution of cluster stats
     distribution: array = full(N_PERMUTATIONS, nan)
@@ -43,10 +49,10 @@ def main(layer: DNNLayer):
     for permutation_i in range(N_PERMUTATIONS):
         shuffled_labels = shuffle(label_array)
         distribution[permutation_i] = (statistic_for_labelling(activations_per_word_phone, shuffled_labels))
+        print_progress(permutation_i + 1, N_PERMUTATIONS)
 
     p_value = 1 - quantile_of_score(distribution, observed_value, kind='strict')
 
-    logger.info(layer.name)
     logger.info(f"\tcluster statistic: {observed_value}")
     logger.info(f"\tp-value for {N_PERMUTATIONS} permutations: {p_value}")
 
@@ -60,11 +66,11 @@ def statistic_for_labelling(activations_per_word_phone: array, labels: array) ->
 
     returns: cluster stat for labelling
     """
-    # TODO: Use cluster statistic code from Chao.
-    return -1
+    return GetFisher(data=activations_per_word_phone, labels=labels)
 
 
 if __name__ == '__main__':
-    basicConfig(format='%(asctime)s | %(levelname)s | %(module)s | %(message)s', datefmt="%Y-%m-%d %H:%M:%S", level=INFO)
+    basicConfig(format='%(asctime)s | %(levelname)s | %(module)s | %(message)s', datefmt="%Y-%m-%d %H:%M:%S",
+                level=INFO)
     for l in DNNLayer:
         main(l)
