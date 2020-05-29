6ab345e86ccc7e613dd3ba7259257cbb85ccf90f
Cai Wingfield <cw417@cam.ac.uk>
2020-05-29 10:58:18 +0100

    Option for Davies–Bouldin indices

--- permutation_p_values.py
+++ permutation_p_values.py
@@ -14,11 +14,13 @@ caiwingfield.net
 2020
 ---------------------------
 """
+from enum import Enum, auto
 from typing import Callable
 from logging import getLogger, basicConfig, INFO
 
 from numpy import array, nan, full
 from sklearn.decomposition import PCA
+from sklearn.metrics import davies_bouldin_score
 
 from common.layers import load_and_stack_data_for_layer, DNNLayer
 from common.logging import print_progress
@@ -32,7 +34,12 @@ N_PERMUTATIONS = 5_000
 PCA_DIMS = 26
 
 
-def statistics_for_class(layer: DNNLayer, class_labelling: Callable[[Phone], int], with_pca: bool):
+class Measure(Enum):
+    Fisher        = auto()
+    DaviesBouldin = auto()
+
+
+def statistics_for_class(layer: DNNLayer, class_labelling: Callable[[Phone], int], measure: Measure, with_pca: bool):
 
     phone_segmentations = PhoneSegmentationSet.load()
 
@@ -51,27 +58,41 @@ def statistics_for_class(layer: DNNLayer, class_labelling: Callable[[Phone], int
     else:
         activations = activations_per_word_phone
 
-    observed_value = statistic_for_labelling(activations, label_array)
+    observed_value = statistic_for_labelling(activations, label_array, measure)
 
-    logger.info(f"\tcluster statistic: {observed_value}")
+    logger.info(f"\tcluster statistic ({measure.name}): {observed_value}")
 
-    # preallocate permutation distribution of cluster stats
-    distribution: array = full(N_PERMUTATIONS, nan)
+    # preallocate permutation null distribution of cluster stats
+    null_distribution: array = full(N_PERMUTATIONS, nan)
 
+    # create null distribution
     for permutation_i in range(N_PERMUTATIONS):
         shuffled_labels = shuffle(label_array)
-        distribution[permutation_i] = (statistic_for_labelling(activations, shuffled_labels))
+        null_distribution[permutation_i] = statistic_for_labelling(activations, shuffled_labels, measure=measure)
         print_progress(permutation_i + 1, N_PERMUTATIONS)
 
-    p_value = 1 - quantile_of_score(distribution, observed_value, kind='strict')
+    # Compute p-values from distribution
+    extra_messages = []
+    if measure == Measure.Fisher:
+        # higher is better
+        p_value = 1 - quantile_of_score(null_distribution, observed_value, kind='strict')
+        if observed_value > max(null_distribution):
+            extra_messages.append(f"statistic ({observed_value}) was largest in null distribution (max={max(null_distribution)})")
+            extra_messages.append(f"this should be noted, and p-value should instead be \"< {1/N_PERMUTATIONS}\"")
+    elif measure == Measure.DaviesBouldin:
+        # lower is better
+        p_value = quantile_of_score(null_distribution, observed_value, kind='strict')
+        if observed_value < min(null_distribution):
+            extra_messages.append(f"statistic ({observed_value}) was smallest in null distribution (min={min(null_distribution)})")
+            extra_messages.append(f"this should be noted, and p-value should instead be \"< {1/N_PERMUTATIONS}\"")
+    else:
+        raise NotImplementedError()
 
     logger.info(f"\tp-value for {N_PERMUTATIONS} permutations: {p_value}")
-    if observed_value > max(distribution):
-        logger.info(f"\t\tstatistic ({observed_value}) was largest in distribution (max={max(distribution)})")
-        logger.info(f"\t\tthis should be noted, and p-value should instead be \"< {1/N_PERMUTATIONS}\"")
+    _ = [logger.info(f"\t\t{m}") for m in extra_messages]
 
 
-def statistic_for_labelling(activations_per_word_phone: array, labels: array) -> float:
+def statistic_for_labelling(activations_per_word_phone: array, labels: array, measure: Measure) -> float:
     """
     Computes a clustering statistic for the set of activations per phone-instance, given a specified set of labels.
 
@@ -80,7 +101,12 @@ def statistic_for_labelling(activations_per_word_phone: array, labels: array) ->
 
     returns: cluster stat for labelling
     """
-    return GetFisher(data=activations_per_word_phone, labels=labels)
+    if measure == Measure.Fisher:
+        return GetFisher(data=activations_per_word_phone, labels=labels)
+    elif measure == Measure.DaviesBouldin:
+        return davies_bouldin_score(X=activations_per_word_phone, labels=labels)
+    else:
+        raise NotImplementedError()
 
 
 if __name__ == '__main__':
@@ -90,10 +116,13 @@ if __name__ == '__main__':
         logger.info(f"=== {l.name} ===")
 
         logger.info("- Phone classification")
-        statistics_for_class(l, class_labelling=lambda phone: phone.value, with_pca=True)
+        statistics_for_class(l, class_labelling=lambda phone: phone.value,
+                             measure=Measure.DaviesBouldin, with_pca=True)
 
         logger.info("- Place/front feature hierarchy classification")
-        statistics_for_class(l, class_labelling=lambda phone: phone.hierarchy_feature_place_front.value, with_pca=True)
+        statistics_for_class(l, class_labelling=lambda phone: phone.hierarchy_feature_place_front.value,
+                             measure=Measure.DaviesBouldin, with_pca=True)
 
         logger.info("- Manner/close feature hierarchy classification")
-        statistics_for_class(l, class_labelling=lambda phone: phone.hierarchy_feature_manner_close.value, with_pca=True)
+        statistics_for_class(l, class_labelling=lambda phone: phone.hierarchy_feature_manner_close.value,
+                             measure=Measure.DaviesBouldin, with_pca=True)
