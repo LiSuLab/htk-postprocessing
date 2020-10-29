51939516ed3c7b61ef5bf1075fac07e7ceedc01d
Cai Wingfield <cw417@cam.ac.uk>
2020-10-29 11:45:58 +0000

    Export all results

--- permutation_p_values.py
+++ permutation_p_values.py
@@ -15,12 +15,14 @@ caiwingfield.net
 ---------------------------
 """
 from collections import namedtuple
+from dataclasses import dataclass
 from enum import Enum, auto
 from pathlib import Path
 from typing import Callable, Optional
 from logging import getLogger, basicConfig, INFO, FileHandler
 
 from numpy import array, nan, full, where
+from pandas import DataFrame
 from sklearn.decomposition import PCA
 from sklearn.metrics import davies_bouldin_score, silhouette_score
 
@@ -42,8 +44,22 @@ class Measure(Enum):
     Dunn          = auto()
 
 
+@dataclass
+class ClusteringResult:
+    pca: bool
+    pca_dims: Optional[int]
+    pca_explained_variance_total: Optional[float]
+    cluster_statistic_type: Measure
+    cluster_statistic_value: float
+    cluster_statistic_p: Optional[float]
+    cluster_statistic_p_perms: Optional[int]
+    cluster_statistic_p_default: Optional[bool]
+
+
 def statistics_for_class(segmentation_path: Path, activations_path: Path, file_pattern: str,
-                         layer: DNNLayer, class_labelling: Callable[[Phone], Optional[int]], measure: Measure, pca_dims: Optional[int], p_value_perms: Optional[int]) -> None:
+                         layer: DNNLayer, class_labelling: Callable[[Phone], Optional[int]], measure: Measure,
+                         pca_dims: Optional[int], p_value_perms: Optional[int]) -> ClusteringResult:
+
 
     with_pca = pca_dims is not None
     compute_p_value = p_value_perms is not None
@@ -69,43 +85,64 @@ def statistics_for_class(segmentation_path: Path, activations_path: Path, file_p
         activations = pca.fit_transform(activations_per_word_phone)
         logger.info(f"\t\tExplained variance: {sum(pca.explained_variance_ratio_)} ({', '.join(list(f'{v:0.2}' for v in pca.explained_variance_ratio_))})")
     else:
+        pca = None
         activations = activations_per_word_phone
 
     observed_value = statistic_for_labelling(activations, label_array, measure)
 
     logger.info(f"\tcluster statistic ({measure.name}): {observed_value}")
 
-    if not compute_p_value:
-        return
+    if compute_p_value:
 
-    # preallocate permutation null distribution of cluster stats
-    null_distribution: array = full(p_value_perms, nan)
+        # preallocate permutation null distribution of cluster stats
+        null_distribution: array = full(p_value_perms, nan)
 
-    # create null distribution
-    for permutation_i in range(p_value_perms):
-        shuffled_labels = shuffle(label_array)
-        null_distribution[permutation_i] = statistic_for_labelling(activations, shuffled_labels, measure=measure)
-        print_progress(permutation_i + 1, p_value_perms)
+        # create null distribution
+        for permutation_i in range(p_value_perms):
+            shuffled_labels = shuffle(label_array)
+            null_distribution[permutation_i] = statistic_for_labelling(activations, shuffled_labels, measure=measure)
+            print_progress(permutation_i + 1, p_value_perms)
 
-    # Compute p-values from distribution
-    extra_messages = []
-    if measure in {Measure.Fisher, Measure.Silhouette, Measure.Dunn}:
-        # higher is better
-        p_value = 1 - quantile_of_score(null_distribution, observed_value, kind='strict')
-        if observed_value > max(null_distribution):
-            extra_messages.append(f"statistic ({observed_value}) was largest in null distribution (max={max(null_distribution)})")
-            extra_messages.append(f"this should be noted, and p-value should instead be \"< {1 / p_value_perms}\"")
-    elif measure == Measure.DaviesBouldin:
-        # lower is better
-        p_value = quantile_of_score(null_distribution, observed_value, kind='strict')
-        if observed_value < min(null_distribution):
-            extra_messages.append(f"statistic ({observed_value}) was smallest in null distribution (min={min(null_distribution)})")
-            extra_messages.append(f"this should be noted, and p-value should instead be \"< {1 / p_value_perms}\"")
+        # Compute p-values from distribution
+        extra_messages = []
+        if measure in {Measure.Fisher, Measure.Silhouette, Measure.Dunn}:
+            # higher is better
+            p_value = 1 - quantile_of_score(null_distribution, observed_value, kind='strict')
+            if observed_value > max(null_distribution):
+                p_fell_off = True
+                extra_messages.append(f"statistic ({observed_value}) was largest in null distribution (max={max(null_distribution)})")
+                extra_messages.append(f"this should be noted, and p-value should instead be \"< {1 / p_value_perms}\"")
+            else:
+                p_fell_off = False
+        elif measure == Measure.DaviesBouldin:
+            # lower is better
+            p_value = quantile_of_score(null_distribution, observed_value, kind='strict')
+            if observed_value < min(null_distribution):
+                p_fell_off = True
+                extra_messages.append(f"statistic ({observed_value}) was smallest in null distribution (min={min(null_distribution)})")
+                extra_messages.append(f"this should be noted, and p-value should instead be \"< {1 / p_value_perms}\"")
+            else:
+                p_fell_off = False
+        else:
+            raise NotImplementedError()
+
+        logger.info(f"\tp-value for {p_value_perms} permutations: {p_value}")
+        _ = [logger.info(f"\t\t{m}") for m in extra_messages]
+
     else:
-        raise NotImplementedError()
+        p_value = None
+        p_fell_off = None
 
-    logger.info(f"\tp-value for {p_value_perms} permutations: {p_value}")
-    _ = [logger.info(f"\t\t{m}") for m in extra_messages]
+    return ClusteringResult(
+        pca=with_pca,
+        pca_dims=pca_dims,
+        pca_explained_variance_total=sum(pca.explained_variance_ratio_) if pca is not None else None,
+        cluster_statistic_type=measure,
+        cluster_statistic_value=observed_value,
+        cluster_statistic_p_perms=p_value_perms if compute_p_value else None,
+        cluster_statistic_p=p_value if compute_p_value else None,
+        cluster_statistic_p_default=p_fell_off if compute_p_value else None,
+    )
 
 
 def statistic_for_labelling(activations_per_word_phone: array, labels: array, measure: Measure) -> float:
@@ -133,25 +170,28 @@ if __name__ == '__main__':
     basicConfig(format='%(asctime)s | %(levelname)s | %(module)s | %(message)s', datefmt="%Y-%m-%d %H:%M:%S", level=INFO)
 
     stat = Measure.DaviesBouldin
-    perms = None#5_000
+    perms = 5_000
     pca = None
 
     root_dir = Path("/Users/cai/Dox/Academic/Analyses/Lexpro/DNN mapping")
     cluster_analysis_root_dir = Path(root_dir, "cluster analysis")
     alignments_root_dir = Path(root_dir, "phonetic alignments")
     activations_dir = Path(root_dir, "extracted activations mat files")
+    csv_path = Path(root_dir, "cluster analysis", "results.csv")
 
-    for s in [3, 4, 5, 0]:
+    results = []
+
+    for system in [3, 4, 5, 0]:
 
         # Set up new log file handler
-        fh = FileHandler(Path(root_dir, "cluster analysis", f"clustering system{s} {stat.name}.log"), "w")
+        fh = FileHandler(Path(root_dir, "cluster analysis", f"clustering system{system} {stat.name}.log"), "w")
         for h in logger.handlers[:]:
             logger.removeHandler(h)
         logger.addHandler(fh)
 
-        logger.info(f" <<< SYSTEM {s} >>> ")
+        logger.info(f" <<< SYSTEM {system} >>> ")
 
-        if s == 0:
+        if system == 0:
             layers = [l for l in DNNLayer]
         else:
             # TODO: dear god, I wasn't expecting to have to adapt this for new network architectures
@@ -175,9 +215,29 @@ if __name__ == '__main__':
             ]:
 
                 logger.info(f"- {name} feature hierarchy classification")
-                statistics_for_class(segmentation_path=Path(alignments_root_dir, f"system{s}", "segmentation"),
-                                     activations_path=Path(activations_dir, f"system{s}"),
-                                     file_pattern="hidden_layer_{0}_activations.mat" if s == 0 else "hmm{0}_activations.mat",
-                                     layer=l, class_labelling=labelling, measure=stat, pca_dims=pca, p_value_perms=perms)
+                clustering_result = statistics_for_class(
+                    segmentation_path=Path(alignments_root_dir, f"system{system}", "segmentation"),
+                    activations_path=Path(activations_dir, f"system{system}"),
+                    file_pattern="hidden_layer_{0}_activations.mat" if system == 0 else "hmm{0}_activations.mat",
+                    layer=l, class_labelling=labelling, measure=stat, pca_dims=pca, p_value_perms=perms)
+
+                results.append({
+                    "System": system,
+                    "Layer name": l.name,
+                    "Layer oldname": l.old_name,
+                    "Layer value": l.value,
+                    "PCA": clustering_result.pca,
+                    "PCA dims": clustering_result.pca_dims,
+                    "PCA explained variance total": clustering_result.pca_explained_variance_total,
+                    "Labelling": name,
+                    "Cluster statistic type": clustering_result.cluster_statistic_type.name,
+                    "Cluster statistic value": clustering_result.cluster_statistic_value,
+                    "Cluster statistic p": clustering_result.cluster_statistic_p,
+                    "Cluster statistic p perms": clustering_result.cluster_statistic_p_perms,
+                    "Cluster statistic p default": clustering_result.cluster_statistic_p_default,
+                })
 
         logger.info("")
+
+    with open(csv_path.absolute(), mode="w", encoding="utf-8") as csv_file:
+        DataFrame(results).to_csv(csv_file, index=False)
